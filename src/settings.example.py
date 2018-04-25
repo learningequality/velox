ef701af2657f0f5c6a4e992055172c40c4d88320
bonidjukic <boni@djukic.com.hr>
2018-04-25 16:36:59 +0200

    Add settings.example.py file to the project

---
+++ src/settings.example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+config = {
+    'db': {
+        'sqlite': {
+            'db_files': ['db.sqlite3', 'db.sqlite3-shm', 'db.sqlite3-wal'],
+        }
+    },
+    'channels': {
+        'mappings': {
+            'no': [],
+            'large': [
+                'b3638b89b1984dbfb6d859c17fc875ee'
+            ],
+            'multiple_small': [
+                '2ac9112b4ec240538e5d3fa93502b51d',
+                '63699b1949224c80b5bb33fc77729433',
+                'b871fb7602f7485983881e9dfe959568',
+                '0410e3b11e894a2e8fd0cde73927cf65',
+                '58c6e3dbca6f454d87334c049c7ba694',
+                'f5c0dacf87964d2aa3922b7e3451f42d',
+                '05436ac33a2d41e1ac600dfdf0e14835',
+                'b2d2019551dd430dbedc938747f0705c',
+                '9be9d91274974a01b1f78d7a38f5b7a6',
+                '56efebc6c0354c7ea0774d445b15194e',
+            ],
+            'video': [
+                'a12c07335aa94c588acdb3cc1ba620dd'
+            ],
+            'exercise': [
+                '1a9158aa082a460b9ea693ba329e0770'
+            ],
+        }
+    }
+}
