5ad1b0a7adebd75dfda0918239d612034bc12c53
José L. Redrejo Rodríguez <jredrejo@debian.org>
2018-04-25 18:06:49 +0200

    Refactoring bootstrap to extract utils functions

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -14,15 +14,14 @@ Usage: python bootstrap.py
 from __future__ import absolute_import, print_function, unicode_literals
 
 import argparse
-import errno
-import os
-import shutil
+
 import subprocess
 
 from datetime import datetime
 
 from filelock import FileLock
-from utils import enable_log_to_stdout
+from utils import copy_clean_db, enable_log_to_stdout, get_kolibri_exec, get_kolibri_venv
+from utils import set_kolibri_home, delete_current_db
 from settings import config
 
 
@@ -33,7 +32,7 @@ def bootstrap():
       - delete the current Kolibri SQLite database
       - copy the database we previously prepared for testing
     """
-    set_kolibri_home()
+    set_kolibri_home(opts, logger)
 
     delete_current_db()
     if copy_clean_db() and opts.channel:
@@ -41,7 +40,7 @@ def bootstrap():
 
 
 def import_channels():
-    kolibri_exec = get_kolibri_exec()
+    kolibri_exec = get_kolibri_exec(opts)
     if kolibri_exec:
         logger.info('Using {} to run Kolibri'.format(kolibri_exec))
     else:
@@ -66,82 +65,10 @@ def import_channels():
                 subprocess.call(call_args)
             except Exception:
                 # try to prepend virtualenv python executable
-                kolibri_venv_python = os.path.join(get_kolibri_venv(), 'bin', 'python')
+                kolibri_venv_python = os.path.join(get_kolibri_venv(opts), 'bin', 'python')
                 call_args.insert(0, kolibri_venv_python)
                 logger.info('Try to prepend virtualenv python executable, running: {}'.format(' '.join(call_args)))
                 subprocess.call(call_args)
-
-
-def copy_clean_db():
-    """
-    Copy the previously prepared testing database to the kolibri_home directory
-    """
-    kolibri_home = get_kolibri_home()
-    db_name = 'db.sqlite3'
-
-    shutil.copyfile(os.path.join('data', db_name),
-                    os.path.join(kolibri_home, db_name))
-    return os.path.exists(os.path.join(kolibri_home, 'db.sqlite3'))
-
-
-def delete_current_db():
-    """
-    Delete the current Kolibri SQLite databases
-    """
-    db_files = ['db.sqlite3', 'db.sqlite3-shm', 'db.sqlite3-wal']
-    for db_file in db_files:
-        try:
-            os.remove(os.path.join(get_kolibri_home(), db_file))
-        except OSError as e:
-            if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
-                raise  # re-raise exception if a different error occurred
-
-
-def set_kolibri_home():
-    """
-    Set the KOLIBRI_HOME environment variable:
-      - if the directory value has been passed as a script argument, use that
-      - set the default value to be the one defined as per the Kolibri code
-    """
-    if opts.kolibri_home:
-        kolibri_home = opts.kolibri_home
-    else:
-        kolibri_home = os.path.join(os.path.expanduser('~'), '.kolibri')
-
-    logger.info('Setting the KOLIBRI_HOME env to {}'.format(kolibri_home))
-    os.environ.setdefault('KOLIBRI_HOME', kolibri_home)
-
-
-def get_kolibri_home():
-    """
-    Return KOLIBRI_HOME environment variable
-    """
-    return os.environ.get('KOLIBRI_HOME')
-
-
-def get_kolibri_venv():
-    """
-    Return the Kolibri virtualenv's path
-    """
-    if opts.kolibri_venv:
-        return opts.kolibri_venv
-    return os.path.join(os.path.expanduser('~'), os.path.join('.venvs', 'kolibri'))
-
-
-def get_kolibri_exec():
-    """
-    Return the path to the Kolibri management command executable
-    """
-    if opts.kolibri_exec:
-        return opts.kolibri_exec
-
-    try:
-        # check if `kolibri` command is avilable and surpress stdout output if it is
-        cmd = 'kolibri'
-        subprocess.call([cmd], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
-        return cmd
-    except FileNotFoundError:
-        return None
 
 
 def fill_parse_args():
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
 
+import errno
 import logging
+import os
+import shutil
 
-__all__ = ['enable_log_to_stdout']
+__all__ = ['copy_clean_db', 'delete_current_db', 'enable_log_to_stdout', 'get_kolibri_venv', 'set_kolibri_home']
 
 
 def enable_log_to_stdout(logname):
@@ -17,3 +20,75 @@ def enable_log_to_stdout(logname):
     handler.setFormatter(formatter)
     log.addHandler(handler)
     return log
+
+
+def copy_clean_db():
+    """
+    Copy the previously prepared testing database to the kolibri_home directory
+    """
+    kolibri_home = get_kolibri_home()
+    db_name = 'db.sqlite3'
+
+    shutil.copyfile(os.path.join('data', db_name),
+                    os.path.join(kolibri_home, db_name))
+    return os.path.exists(os.path.join(kolibri_home, 'db.sqlite3'))
+
+
+def delete_current_db():
+    """
+    Delete the current Kolibri SQLite databases
+    """
+    db_files = ['db.sqlite3', 'db.sqlite3-shm', 'db.sqlite3-wal']
+    for db_file in db_files:
+        try:
+            os.remove(os.path.join(get_kolibri_home(), db_file))
+        except OSError as e:
+            if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+                raise  # re-raise exception if a different error occurred
+
+
+def set_kolibri_home(opts, logger):
+    """
+    Set the KOLIBRI_HOME environment variable:
+      - if the directory value has been passed as a script argument, use that
+      - set the default value to be the one defined as per the Kolibri code
+    """
+    if opts.kolibri_home:
+        kolibri_home = opts.kolibri_home
+    else:
+        kolibri_home = os.path.join(os.path.expanduser('~'), '.kolibri')
+
+    logger.info('Setting the KOLIBRI_HOME env to {}'.format(kolibri_home))
+    os.environ.setdefault('KOLIBRI_HOME', kolibri_home)
+
+
+def get_kolibri_home():
+    """
+    Return KOLIBRI_HOME environment variable
+    """
+    return os.environ.get('KOLIBRI_HOME')
+
+
+def get_kolibri_venv(opts):
+    """
+    Return the Kolibri virtualenv's path
+    """
+    if opts.kolibri_venv:
+        return opts.kolibri_venv
+    return os.path.join(os.path.expanduser('~'), os.path.join('.venvs', 'kolibri'))
+
+
+def get_kolibri_exec():
+    """
+    Return the path to the Kolibri management command executable
+    """
+    if opts.kolibri_exec:
+        return opts.kolibri_exec
+
+    try:
+        # check if `kolibri` command is avilable and surpress stdout output if it is
+        cmd = 'kolibri'
+        subprocess.call([cmd], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
+        return cmd
+    except FileNotFoundError:
+        return None
