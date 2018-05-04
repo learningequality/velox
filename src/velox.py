34283402eeb9943dabecb5a222a9b6e6e99ef361
bonidjukic <boni@djukic.com.hr>
2018-05-04 17:07:47 +0200

    Use the correct opts helper name in velox

--- src/velox.py
+++ src/velox.py
@@ -39,7 +39,7 @@ from bootstrap import bootstrap_database
 from requests.exceptions import RequestException
 from utils import calculate_duration
 from utils import enable_log_to_stdout, get_free_tcp_port
-from utils import set_kolibri_home, get_config_args, manage_cli, select_cli, prepare_postgresql_connection
+from utils import set_kolibri_home, get_config_opts, manage_cli, select_cli, prepare_postgresql_connection
 from utils import show_error
 
 
@@ -199,7 +199,7 @@ if __name__ == '__main__':
     start_date = datetime.utcnow()
     wanted_args = ['kolibri_dev', 'kolibri_venv', 'kolibri_exec', 'database', 'channel',
                    'iterations', 'learners', 'classrooms', 'test']
-    opts = get_config_args(wanted=wanted_args, description='Velox setup script')
+    opts = get_config_opts(wanted=wanted_args, description='Velox setup script')
     log_name = 'setup_tests'
     logger = enable_log_to_stdout(log_name)
     tests_durations = {}
