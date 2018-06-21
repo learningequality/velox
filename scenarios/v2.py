35a3f05ecf8e602924869dca95640e0bb442e484
bonidjukic <boni@djukic.com.hr>
2018-06-21 13:52:59 +0200

    Move locust files to separate lib.locust modules

--- scenarios/v2.py
+++ scenarios/v2.py
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Experiment scenario 2
-Server unresponsive when multiple clients access different resource
-
-Endpoints (shared between the requests):
-* /user/#/signin
-* /facility/#/classes
-* /learn/#/topics/{id} : video
-* /learn/#/topics/{id} : pdf
-* /learn/#/topics/{id} : JavaScript (Phet or similars)
-* … exercises, exams, coach pages
-
-
-Channels    Learners        Classrooms     Requests/s   Database
-================================================================
-No              25              2           20          sqlite
-large           25              2           20          sqlite
-video           25              2           20          sqlite
-video           25              2           100         sqlite
-video           25              2           100         postgresql
-Multiple        25              2           20          sqlite
-Multiple        25              2           100         sqlite
-Multiple        25              2           100         postgresql
+Testing / temporary scenario 2
 """
 from __future__ import print_function, unicode_literals
 import os
@@ -32,8 +11,6 @@ try:
     from lib.locust.user import KolibriUserBehavior, AdminUser
     from lib.locust.wrapper import launch
 except ImportError:
-    # the test is being run out of velox environment
-    # and velox package is not installed
     import sys
     sys.path.append(os.path.join(os.getcwd(), 'src'))
     from lib.locust.user import KolibriUserBehavior, AdminUser
@@ -42,27 +19,27 @@ except ImportError:
 admin = AdminUser(base_url=os.environ.get('KOLIBRI_BASE_URL', 'http://127.0.0.1:8000'))
 KolibriUserBehavior.KOLIBRI_USERS = admin.get_users()
 KolibriUserBehavior.KOLIBRI_RESOURCES = admin.get_resources()
-KolibriUserBehavior.RANDOMIZE = False
+
 
 class UserBehavior(KolibriUserBehavior):
 
-    @task(30)
+    @task(3)
     def browse_resources(self):
         self.browse_resource()
 
-    @task(40)
+    @task(4)
     def load_video_resources(self):
         self.load_resource('video')
 
-    @task(50)
+    @task(5)
     def load_html5_resources(self):
         self.load_resource('html5')
 
-    @task(20)
+    @task(2)
     def load_document_resources(self):
         self.load_resource('document')
 
-    @task(30)
+    @task(3)
     def load_exercise_resources(self):
         self.load_resource('exercise')
 
@@ -74,8 +51,16 @@ class WebsiteUser(HttpLocust):
     max_wait = 0
 
 
+def on_request_failure(request_type, name, response_time, response_length):
+    """
+    Event handler that get triggered on every successful request
+    """
+    print(request_type)
+
+
 def run(learners=30):
-    launch(WebsiteUser, learners, run_time=120)
+    rate = 5
+    launch(WebsiteUser, learners, rate, run_time=60)
 
 
 if __name__ == '__main__':
