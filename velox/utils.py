4b8f8512a5a9a7657f233955752e59c9d63188f0
José L. Redrejo Rodríguez <jredrejo@debian.org>
2020-01-22 21:28:01 +0100

    Refactoring utils and velox main files, adding type hints

--- velox/utils.py
+++ velox/utils.py
@@ -6,25 +6,18 @@ from __future__ import print_function, unicode_literals
 
 import argparse
 import logging
-import os
-import socket
-import subprocess
 import sys
 import time
 import traceback
 
-from datetime import datetime
-from string import Template
+from datetime import datetime, timedelta
+from typing import Any, Dict, List, Mapping, Optional
 
-__all__ = ['calculate_duration', 'enable_log_to_stdout', 'get_config_opts', 'get_free_tcp_port', 'manage_cli',
-           'set_kolibri_home', 'select_cli', 'show_error', 'import_postgresql_dump', 'export_postgresql_dump',
-           'write_options_ini', 'add_timestamp']
-
-if sys.version_info < (3,):
-    FileNotFoundError = IOError
+__all__ = ['calculate_duration', 'enable_log_to_stdout', 'get_config_opts',
+           'show_error', 'add_timestamp']
 
 
-def enable_log_to_stdout(logname):
+def enable_log_to_stdout(logname: str) -> logging.Logger:
     """Given a log name, outputs > INFO to stdout."""
     log = logging.getLogger(logname)
     log.setLevel(logging.DEBUG)
@@ -37,156 +30,26 @@ def enable_log_to_stdout(logname):
     return log
 
 
-def set_kolibri_home(path, logger):
-    """
-    Set the KOLIBRI_HOME environment variable:
-      - if the directory value has been passed as an argument, use that
-      - set the default value to be the one defined as per the Kolibri code
-    """
-    kolibri_home = path if path else os.path.join(os.path.expanduser('~'), '.kolibri')
-    logger.info('Setting the KOLIBRI_HOME env to {}'.format(kolibri_home))
-    os.environ['KOLIBRI_HOME'] = kolibri_home
-
-
-def get_kolibri_home():
-    """
-    Return KOLIBRI_HOME environment variable
-    """
-    return os.environ.get('KOLIBRI_HOME')
-
-
-def get_kolibri_module(opts):
-    """
-    Return the path to the kolibri module within the Kolibri developmnent installation directory
-    i.e. [kolibri_dev]/kolibri
-    """
-    if not opts.kolibri_dev:
-        raise ValueError('kolibri-dev argument is missing')
-    return os.path.join(opts.kolibri_dev, 'kolibri')
-
-
-def get_kolibri_venv_python(opts):
-    """
-    Return the path to the python executable within the Kolibri virtualenv
-    i.e. [kolibri_venv]/bin/python
-    """
-    if not opts.kolibri_venv:
-        raise ValueError('kolibri-venv argument is missing')
-    return os.path.join(opts.kolibri_venv, 'bin', 'python')
-
-
-def get_free_tcp_port():
-    """
-    Find a free tcp port to run Kolibri server
-    """
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.bind(('', 0))
-    _, port = tcp.getsockname()
-    tcp.close()
-    return port
-
-
-def select_cli(opts):
-    """
-    Returns the right way to call kolibri execution,
-    depending on the provided opts
-    """
-    if opts.kolibri_exec:
-        commands = [opts.kolibri_exec, ]
-    elif opts.kolibri_dev:
-        python_exec = get_kolibri_venv_python(opts)
-        kolibri_module = get_kolibri_module(opts)
-        commands = [python_exec, kolibri_module]
-    else:
-        commands = ['kolibri', ]
-    return commands
-
-
-def manage_cli(opts, *args):
-    """
-    Returns the right way to execute kolibri manage commands,
-    depending on the provided opts
-    """
-    commands = select_cli(opts)
-
-    return commands + ['manage', ] + list(args)
-
-
-def import_postgresql_dump(dump_path, opts, logger):
-    try:
-        insert_cmd = ['psql',
-                      '-h', opts.db_postgresql_host,
-                      '-U', opts.db_postgresql_user,
-                      '-d', opts.db_postgresql_name,
-                      '-f', dump_path]
-        subprocess.Popen(insert_cmd, env={'PGPASSWORD': opts.db_postgresql_password}).wait()
-        logger.info('Postgres database dump imported from: {}'.format(dump_path))
-        return True
-    except Exception:
-        logger.error('Error trying to dump Postgres database')
-        return False
-
-
-def export_postgresql_dump(dump_path, opts, logger):
-    dump_cmd = ['pg_dump',
-                '-U', opts.db_postgresql_user,
-                '-h', opts.db_postgresql_host,
-                opts.db_postgresql_name,
-                '--clean',
-                '-f', dump_path]
-    subprocess.Popen(dump_cmd, env={'PGPASSWORD': opts.db_postgresql_password}).wait()
-
-    if not os.path.exists(dump_path):
-        logger.error('Error trying to dump Postgres database')
-        return False
-
-    logger.info('Postgres database dump exported to: {}'.format(dump_path))
-    return True
-
-
-def write_options_ini(template, dest, options, logger):
-    """
-    Render options.ini from a template file (located within the resources directory)
-    and write it to the specified destination directory, using the passed `options`
-    dictionary for the config options
-    """
-    tmpl_filename = 'options.{}.ini'.format(template)
-    logger.info('Using {} as options.ini template'.format(tmpl_filename))
-    tmpl_path = open(os.path.join('resources', tmpl_filename))
-    tmpl = Template(tmpl_path.read())
-
-    dest_path = os.path.join(dest, 'options.ini')
-    with open(dest_path, 'w') as ini:
-        ini.write(tmpl.substitute(options))
-
-    if not os.path.exists(dest_path):
-        logger.error('Error trying to write options.ini')
-        return False
-
-    logger.info('options.ini was rendered and written to: {}'.format(dest_path))
-    return True
-
-
-def calculate_duration(start):
+def calculate_duration(start: datetime) -> float:
     """
     Returns the difference in seconds between start and now
     :param: start: datetime to begin
     :returns: Seconds between start and end
     """
-    timing = datetime.utcnow() - start
-    duration = timing.seconds + timing.microseconds / 1000000.0
+    timing: timedelta = datetime.utcnow() - start
+    duration: float = timing.seconds + timing.microseconds / 1000000.0
     return duration
 
 
-def show_error(logger, error, message=''):
-    error_text = str(error) if not hasattr(error, 'message') else error.message
+def show_error(logger: logging.Logger, error: Exception, message: str = ''):
+    error_text: str = str(error)
     if message:
         error_text = '{} {}'.format(error_text, message)
     logger.error(error_text)
     traceback.print_exc(file=sys.stdout)
 
 
-def get_config_opts(wanted, **kwargs):
+def get_config_opts(wanted: List[Optional[str]], **kwargs: str) -> argparse.Namespace:
     """
     Returns the `argparse.Namespace` object by taking into account
     the following prioritization order, from higher to lower:
@@ -200,10 +63,12 @@ def get_config_opts(wanted, **kwargs):
         config = None
 
     # Get command line arguments with the argparse module
-    opts = fill_parse_args(wanted, **kwargs)
-    args_definitions = get_parse_args_definitions()
+    opts: argparse.Namespace = fill_parse_args(wanted, **kwargs)
+    args_definitions: Dict[str, List] = get_parse_args_definitions()
 
     # We assume that all the options are listed in the `get_default_args` function
+    opt_key: str
+    default_value: Any
     for opt_key, default_value in get_default_args().items():
         # Command line arguments have the highest priority
         if opt_key not in opts or not getattr(opts, opt_key, None):
@@ -222,7 +87,7 @@ def get_config_opts(wanted, **kwargs):
         if opt_key in args_definitions:
             try:
                 # Here we're assuming the position of the args configuration dict
-                args_conf = args_definitions[opt_key][2]
+                args_conf: Optional[Any] = args_definitions[opt_key][2]
             except KeyError:
                 # And try one more time if the short arg format was not used
                 args_conf = args_definitions[opt_key][1]
@@ -236,39 +101,32 @@ def get_config_opts(wanted, **kwargs):
     return opts
 
 
-def get_default_args():
+def get_default_args() -> Dict[str, Any]:
     return {
-        'kolibri_dev': '',
-        'kolibri_venv': os.path.join(os.path.expanduser('~'), os.path.join('.venvs', 'kolibri')),
-        'kolibri_exec': '',
-        'database': 'sqlite',
-        'channel': 'multiple',
         'learners': 30,
-        'classrooms': 1,
         'test': 'all',
         'iterations': 3,
-        'db_postgresql_name': '',
-        'db_postgresql_user': '',
-        'db_postgresql_password': '',
-        'db_postgresql_host': '127.0.0.1'
     }
 
 
-def fill_parse_args(wanted, **kwargs):
+def fill_parse_args(wanted: List[Optional[str]], **kwargs: str):
     """
     Create the setup to parse args for the application scripts
     :param: wanted: list of args the application will use
     :returns: A parser object to be used by the script
     """
     if not wanted:
-        wanted = kwargs.get('wanted')
-    description = kwargs.get('description', None)
+        wanted = [kwargs.get('wanted'), ]
+    description: Optional[str] = kwargs.get('description', None)
 
-    parser = argparse.ArgumentParser(description)
-    args_definitions = get_parse_args_definitions(wanted)
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description)
+    args_definitions: Dict[str, List] = get_parse_args_definitions(wanted)
 
     for _, arg_definition in args_definitions.items():
         try:
+            arg_short: str
+            arg_long: str
+            arg_opts: Mapping[str, str]
             arg_short, arg_long, arg_opts = arg_definition
             parser.add_argument(arg_short, arg_long, **arg_opts)
         except ValueError:
@@ -277,50 +135,16 @@ def fill_parse_args(wanted, **kwargs):
     return parser.parse_args()
 
 
-def get_parse_args_definitions(wanted=None):
+def get_parse_args_definitions(wanted: Optional[List[Optional[str]]] = None) -> Dict[str, List]:
     """
     Parse the args the script neeeds
     :param: wanted: list of args the application will use
     :returns: A list with the options for the wanted args
     """
     definitions = {
-        'kolibri_dev': [
-            '-kd', '--kolibri-dev', {
-                'required': False, 'help': 'path to the Kolibri development installation'
-            },
-        ],
-        'kolibri_venv': [
-            '-kv', '--kolibri-venv', {
-                'required': False, 'help': 'path to the Kolibri virtualenv'
-            }
-        ],
-        'kolibri_exec': [
-            '-ke', '--kolibri-exec', {
-                'required': False, 'help': 'command to execute Kolibri cli'
-            }
-        ],
-        'database': [
-            '-d', '--database', {
-                'required': False, 'choices': ['sqlite', 'postgresql'],
-                'help': 'Database type: sqlite or posgresql'
-            }
-        ],
-        'channel': [
-            '-c', '--channel', {
-                'required': False, 'choices': ['large', 'multiple', 'video', 'exercise'],
-                'help': 'Channels to use in Kolibri: large (1 large channel ~ 1Gb),\n'
-                        'multiple (10 x ~30 Mb channels), video (channel with multiple videos),\n'
-                        'exercise (channel with multiple exercises)'
-            }
-        ],
         'learners': [
             '-l', '--learners', {
                 'required': False, 'type': int, 'help': 'Number of learners per classroom that will use the tests'
-            }
-        ],
-        'classrooms': [
-            '-s', '--classrooms', {
-                'required': False, 'type': int, 'help': 'Number of classrooms to be created.'
             }
         ],
         'test': [
@@ -340,9 +164,9 @@ def get_parse_args_definitions(wanted=None):
 
     return definitions
 
-def add_timestamp(url, first=False):
-    time_token = str(time.time()).replace('.', '')[:13]
-    separator = '?' if first else '&'
-    new_url = '{url}{separator}{timestamp}={timestamp}'.format(
+def add_timestamp(url: str, first: bool = False) -> str:
+    time_token: str = str(time.time()).replace('.', '')[:13]
+    separator: str = '?' if first else '&'
+    new_url: str = '{url}{separator}{timestamp}={timestamp}'.format(
         url=url, separator=separator, timestamp=time_token)
     return new_url
--- velox/velox.py
+++ velox/velox.py
@@ -29,23 +29,19 @@ import gevent.monkey
 gevent.monkey.patch_all()  # noqa
 
 import os
-import requests
-import shutil
 import subprocess
 import sys
 import tempfile
-import time
 
 from datetime import datetime
 from filelock import FileLock
 from importlib import import_module
+from typing import Dict, List
 
-from bootstrap import bootstrap_database
-from requests.exceptions import RequestException
 from utils import calculate_duration
 from utils import enable_log_to_stdout, get_free_tcp_port
-from utils import (set_kolibri_home, get_config_opts, manage_cli, select_cli, write_options_ini,
-                   import_postgresql_dump)
+from utils import (get_config_opts, manage_cli,)
+
 from utils import show_error
 
 
@@ -72,55 +68,6 @@ class EnvironmentSetup(object):
         self.base_url = 'http://127.0.0.1:{}'.format(self.port)
         self._instance = None
 
-    def __set_database(self):
-        """
-        Uses the bootstraped data and database to setup a new
-        Kolibri working environment
-        """
-        channel_dir = os.path.join('data', self.opts.channel)
-        bootstrap_database(self.opts, self.logger)
-        self.logger.info('Copying bootstrapped data from {} to {}'.format(channel_dir, self.working_dir))
-        shutil.copytree(channel_dir, self.working_dir)
-        self.__inject_options_ini()
-        set_kolibri_home(self.working_dir, self.logger)
-        if opts.database == 'postgresql':
-            self.__import_dump()
-
-    def __import_dump(self):
-        dump_path = os.path.join(self.working_dir, '{}.sql'.format(self.opts.channel))
-        return import_postgresql_dump(dump_path, self.opts, self.logger)
-
-    def __inject_options_ini(self):
-        """
-        Renders and injects options.ini configuration file into the current working directory
-        """
-        options = vars(self.opts)
-        options.update({'content_dir': self.working_dir})
-        return write_options_ini(self.opts.database, self.working_dir, options, self.logger)
-
-    def do_setup(self):
-        """
-        Prepare all the envirnoment to be able to run Kolibri and tests
-        """
-        self.__set_database()
-
-    def do_clean(self, error_exit=False):
-        """ Finishes Kolibri server and deletes all the temp files used
-        to create the running environment
-        """
-        if self._instance:
-            self._instance.kill()
-        self.logger.info('Kolibri server has been stopped')
-        try:
-            shutil.rmtree(os.path.split(self.working_dir)[0])
-            self.logger.info('Temp working directory has been deleted')
-        except IOError:
-            self.logger.error('Error trying to remove the working directory')
-        except OSError:
-            pass
-        if error_exit:
-            sys.exit(1)
-
     def manage(self, *args):
         """
         Handler to run kolibri manage to invoke a Django command
@@ -130,40 +77,6 @@ class EnvironmentSetup(object):
             subprocess.Popen(call_args).wait()
         except Exception as error:
             show_error(self.logger, error)
-
-    def start(self):
-        """
-        Starts Kolibri web server
-        """
-        try:
-            kolibri_commands = select_cli(self.opts) + ['start', '--port={}'.format(self.port), '--foreground']
-            if opts.kolibri_dev:
-                self.logger.warn('Running kolibri from dev environment. Ensure you have run `yarn build` before')
-            self._instance = subprocess.Popen(kolibri_commands)
-            self._wait_for_server_start()
-
-            self.logger.info('Kolibri server started and running in port {}'.format(self.port))
-            return True
-        except OSError:
-            self.logger.error('Kolibri not found. Tests can\'t continue')
-        except Exception:
-            self.logger.error('Kolibri server did not start')
-        return False
-
-    def _wait_for_server_start(self, timeout=20):
-        """
-        Pauses this script until the web server is serving requests
-        """
-        for _ in range(timeout * 2):
-            try:
-                resp = requests.get(self.base_url, timeout=3)
-                if resp.status_code > 0:
-                    return
-            except RequestException:
-                pass
-            time.sleep(0.5)
-
-        raise Exception('Server did not start within {} seconds'.format(timeout))
 
     def load_tests(self):
         """
@@ -197,21 +110,17 @@ class EnvironmentSetup(object):
 
 if __name__ == '__main__':
     start_date = datetime.utcnow()
-    wanted_args = ['kolibri_dev', 'kolibri_venv', 'kolibri_exec', 'database', 'channel',
-                   'iterations', 'learners', 'classrooms', 'test']
+    wanted_args: List[str] = ['iterations', 'learners', 'test']
     opts = get_config_opts(wanted=wanted_args, description='Velox setup script')
     log_name = 'setup_tests'
     logger = enable_log_to_stdout(log_name)
-    tests_durations = {}
+    tests_durations: Dict[str, List[int]] = {}
     # add scenarios directory to the sys path:
     sys.path.append(os.path.join(os.getcwd(), 'scenarios'))
     with FileLock('{}.lock'.format(log_name)):
         try:
             logger.info('Tests setup script started')
             es = EnvironmentSetup(opts, logger)
-            es.do_setup()
-            if not es.start():
-                es.do_clean(True)
             os.environ['KOLIBRI_BASE_URL'] = es.base_url
             for test in es.load_tests():
                 # Each test is done three times
@@ -226,11 +135,9 @@ if __name__ == '__main__':
                     except AttributeError:
                         logger.error('{} is not a correct module to run tests'.format(test.__name__))
                         # Clean temp files and quit velox:
-                        es.do_clean(True)
                     except Exception as error:
                         show_error(logger, error, 'when trying to run {}'.format(test.__name__))
                     tests_durations[test.__name__].append(calculate_duration(test_start))
-            es.do_clean()
             duration = calculate_duration(start_date)
             logger.info('::Duration {}'.format(duration))
             logger.info('Tests finished')
